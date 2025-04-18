3c790fc6d453964087a69fdc9970b189c8618ef1
eunjaegu <kuhj1021@gmail.com>
2025-04-18 13:39:12 +0900

    ✨ Flask와 JinjaTemplate 챌린지
    Flask와 JinjaTemplate 챌린지 미션

---
+++ Part1/04.Route/app.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, Response
+
+app = Flask(__name__)
+
+# 기본 경로에 대한 라우트
+@app.route('/')
+def home():
+    return "Hello, This is Main Page!"
+
+@app.route('/about')
+def about():
+    return "This is the about page!"
+
+#동적으로 URL 파라미터 값을 받아서 처리해준다.
+#http://127.0.0.1:5000/user/eunjae
+@app.route('/user/<username>')
+def user_profile(username):
+    return f'UserName : {username}'
+
+@app.route('/number/<int:number>')
+def number(number):
+    return f'number : {number}'
+
+#Post 요청 날리는 법
+# (1) postman
+# (2) requests
+import requests # pip install requests
+@app.route('/test')
+def test():
+    url = 'http://127.0.0.1:5000/submit'
+    data = 'test data'
+    response = requests.post(url=url, data=data)
+
+    return response.text
+
+@app.route('/submit', methods=['GET', 'POST', 'PUT', 'DELETE'])
+def submit():
+    print(request.method)
+
+    if request.method == 'GET':
+        print("GET method")
+
+    if request.method == 'POST':
+        print("***POST method***", request.data)
+
+    return Response("Sucessfully submitted", status=200) 
+
+if __name__ == "__main__":
+    app.run()
+
+
